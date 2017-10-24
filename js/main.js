beb86a8aa6ae3530cf6ba0a1ff3a787a0d798bdb
MaksimRadziuk <30619681+MaksimRadziuk@users.noreply.github.com>
2017-10-25 01:22:24 +0300

    25/10 1.22

--- js/main.js
+++ js/main.js
@@ -144,3 +144,39 @@ $(document).ready(function(){
   });
 });
 
+
+
+//использование слик-карусели(карточка товара)
+$('.carusel-block').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1025,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 641,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 501,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+      }
+    }
+  ]
+});
